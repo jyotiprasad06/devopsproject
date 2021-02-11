8d28167e226b60e7dd906077917b975c737f9286
jpmahali18@gmail.com <jpmahali18@gail.com>
2021-02-11 13:46:10 +0000

    adding new project

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,4 +1,4 @@
-package io.github.georgwittberger.springbootembeddedspaexample;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
